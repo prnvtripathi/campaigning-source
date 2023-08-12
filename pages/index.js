b768843c31ee559d6f2d159836ccd075ea9743cb
Pranav Tripathi <tripathipranav14@gmail.com>
2023-08-12 19:49:04 +0530

    tailwind fix

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
 import Layout from "@/components/Layout"
-import Head from "next/head"
 
 const Home = () => {
   return (
-    <>
-    <Head>
-      <title>Campaigning Source</title>
-    </Head>
-      <Layout>
-        <h1>Home</h1>
-      </Layout>
-    </>
+    <Layout>
+      <h1>Home</h1>
+    </Layout>
   )
 }
+
 
 export default Home
