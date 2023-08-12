867459457533467e1db14da6c39e2b8871ef9f8e
Pranav Tripathi <tripathipranav14@gmail.com>
2023-08-12 12:35:07 +0530

    Remove boilerplate

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Campaigning Source</title>
+        <meta name="description" content="A website to find services for your next campaign." />
+        </Head>
       <body>
         <Main />
         <NextScript />
