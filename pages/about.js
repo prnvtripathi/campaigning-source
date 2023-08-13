bb1dc25aefa23cda4c32dbe98a99ab4eb7839a98
Pranav Tripathi <tripathipranav14@gmail.com>
2023-08-13 10:59:58 +0530

    update about

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>
+        </div>
+    )
+}
+
+export default About
