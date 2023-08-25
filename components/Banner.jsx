2bd4442aa17cd6216afd90e8ae19d556aa8cbc13
Pranav Tripathi <tripathipranav14@gmail.com>
2023-08-25 17:26:18 +0530

    update: add banner

---
+++ components/Banner.jsx
@@ -0,0 +1,22 @@
+import { Montserrat, Satisfy } from "next/font/google"
+
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    weights: ['400']
+})
+
+const satisfy = Satisfy({
+    subsets: ['latin'],
+    weight: ['400']
+})
+
+const Banner = () => {
+    return (
+        <div className="py-20 text-center bg-gradient-to-r from-violet-200 to-pink-200 rounded-md">
+            <h1 className="text-6xl"><span className={montserrat.className}>Campaigning Source</span></h1>
+            <p className="text-2xl py-5"><span className={satisfy.className}>Campaigning made easy.</span></p>
+        </div>
+    )
+}
+
+export default Banner
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import SocialMedia from "@/components/SocialMedia"
 import Contact from "@/components/Contact"
 import Head from "next/head"
 import PastCustomers from "@/components/PastCustomers"
+import Banner from "@/components/Banner"
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
         <SocialMedia />
         <Hero />
         <PastCustomers />
+        <Banner />
         <Contact />
       </Layout>
     </>
