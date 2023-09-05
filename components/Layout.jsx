c50713397321cb7a05267aa3e575b8e1bfcc398c
Pranav Tripathi <tripathipranav14@gmail.com>
2023-09-05 19:17:13 +0530

    update: navbar update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,27 +6,36 @@ import Footer from "./Footer"
 const Layout = ({ children }) => {
     return (
         <>
-            <div className='flex justify-around items-center px-6 mb-4'>
-                <div className='p-4 '>
-                    <Link href="/">
-                        <Image
-                            src={logo}
-                            alt="logo"
-                            width={225}
-                            height={225}
-                        />
-                    </Link>
+            <div className="offers">
+                <h3>Boost your business in this festive season to the new heights. 🗻</h3>
+            </div>
+            <div className="content">
+                <div className='flex justify-around items-center px-6 mb-4'>
+                    <div className='p-4 '>
+                        <Link href="/">
+                            <Image
+                                src={logo}
+                                alt="logo"
+                                width={225}
+                                height={225}
+                            />
+                        </Link>
+                    </div>
+                    <ul className="navbar">
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="/contact">Influencers</Link></li>
+                        <li><Link href="/about">About Us</Link></li>
+                        <li><Link href="/contact">Services</Link></li>
+                        <li><Link href="/contact">Pricing</Link></li>
+                        <li><Link href="/contact">Offers</Link></li>
+                        <li><Link href="/contact">Internship</Link></li>
+                    </ul>
                 </div>
-                <ul className="navbar">
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/about">About</Link></li>
-                    <li><Link href="/contact">Contact Us</Link></li>
-                </ul>
+                <div className='container'>
+                    {children}
+                </div>
+                <Footer />
             </div>
-            <div className='container'>
-                {children}
-            </div>
-            <Footer />
         </>
     )
 }
