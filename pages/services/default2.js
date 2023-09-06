1b7e668eb5c3e771992729fa34ad0e357dd23079
Pranav Tripathi <tripathipranav14@gmail.com>
2023-09-06 19:14:27 +0530

    update: design changes

--- pages/services/default2.js
+++ pages/services/default2.js
@@ -3,7 +3,9 @@ import Layout from '@/components/Layout'
 import Image from 'next/image'
 import webdev from '@/assets/services/webdev.png'
 import appdev from '@/assets/services/appdev.png'
+import default2 from '@/assets/services/default2.png'
 import Link from 'next/link'
+import Banner from '@/components/Banner'
 
 const Default2 = () => {
     return (
@@ -14,28 +16,40 @@ const Default2 = () => {
             <Layout>
                 <div>
                     <h1 className="title">Software Development</h1>
-                    <div className="flex gap-4 mb-6 p-4 rounded-md">
-                        <div className="flex flex-col justify-around items-center bg-[#F6F1F1]">
-                            <h2 className='sub-title'>Web Development</h2>
-                            <Image src={webdev} alt="web development" width={360} height={360} className='mt-2 mb-3' />
-                            <p className='text-md w-1/2 my-3'>
-                                We build beautiful, responsive and feature-rich web based applications that solve real customer problems.
-                            </p>
-                            <Link href="/services/webdev" className='bg-[#242424] p-3 rounded text-secondaryText hover:scale-95 my-4 transition duration-100 ease-in-out'>
-                                Learn more
-                            </Link>
+                    <div className='grid grid-cols-5'>
+                        <div className='col-span-3 row-span-2'>
+                            <Image src={default2} alt="default2" width={720} />
                         </div>
-                        <div className="flex flex-col justify-around items-center bg-[#F6F1F1]">
-                            <h2 className='sub-title'>App Development</h2>
-                            <Image src={appdev} alt="app development" width={360} height={360} className='mt-2 mb-3' />
-                            <p className='text-md w-1/2 my-3'>
-                                We build Android, iOS and PWA's that work great across multiple devices, screen sizes and resolutions.
-                            </p>
-                            <Link href="/services/appdev" className='bg-[#242424] p-3 rounded text-secondaryText hover:scale-95 my-4 transition duration-100 ease-in-out'>
-                                Learn more
-                            </Link>
+                        <div className='col-span-2'>
+                            <div className="flex flex-col gap-4 mb-6 p-4 rounded-md">
+                                <div className="flex flex-col justify-around items-center bg-[#F6F1F1]">
+                                    <h2 className='sub-title'>Web Development</h2>
+                                    <div className='flex justify-around items-center'>
+                                        <Image src={webdev} alt="web development" width={180} height={180} className='mt-2 mb-3' />
+                                        <p className='text-md w-1/2 my-3 text-center'>
+                                            We build beautiful, responsive and feature-rich web based applications that solve real customer problems.
+                                        </p>
+                                    </div>
+                                    <Link href="/services/webdev" className='bg-[#242424] p-3 rounded text-secondaryText hover:scale-95 my-4 transition duration-100 ease-in-out'>
+                                        Learn more
+                                    </Link>
+                                </div>
+                                <div className="flex flex-col justify-around items-center bg-[#F6F1F1]">
+                                    <h2 className='sub-title'>App Development</h2>
+                                    <div className='flex justify-around items-center'>
+                                        <Image src={appdev} alt="app development" width={180} height={180} className='mt-2 mb-3' />
+                                        <p className='text-md w-1/2 my-3 text-center'>
+                                            We build Android, iOS and PWA's that work great across multiple devices, screen sizes and resolutions.
+                                        </p>
+                                    </div>
+                                    <Link href="/services/appdev" className='bg-[#242424] p-3 rounded text-secondaryText hover:scale-95 my-4 transition duration-100 ease-in-out'>
+                                        Learn more
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <Banner />
                 </div>
             </Layout>
         </>
