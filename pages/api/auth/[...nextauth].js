b43817c3d2d5e4a44d1ba6b7dec7acae184a902d
Pranav Tripathi <tripathipranav14@gmail.com>
2023-09-13 16:20:22 +0530

    update: chores

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
-// import EmailProvider from 'next-auth/providers/email'
+import EmailProvider from 'next-auth/providers/email'
 
 export default NextAuth({
   providers: [
@@ -11,11 +11,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-    // Passwordless / email sign in
-    // EmailProvider({
-    //   server: process.env.MAIL_SERVER,
-    //   from: 'NextAuth.js <no-reply@example.com>'
-    // }),
   ],
   adapter: MongoDBAdapter(clientPromise), // Adapter (recommended)
 })
