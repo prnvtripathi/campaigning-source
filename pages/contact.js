1a1bed96dd31e334f1e9e35cd686466973f00988
Pranav Tripathi <tripathipranav14@gmail.com>
2024-01-12 22:51:22 +0530

    update contact form

--- pages/contact.js
+++ pages/contact.js
@@ -5,7 +5,7 @@ import emailjs from "@emailjs/browser";
 import Link from "next/link";
 import {
     FaFacebook,
-    FaGithub,
+    FaLinkedinIn,
     FaInstagram,
     FaTwitter,
 } from "react-icons/fa";
@@ -50,49 +50,57 @@ const Contact = () => {
                 <title>Contact Us | Campaigning Source</title>
             </Head>
             <Layout>
-                <div className="grid grid-cols-1 md:grid-cols-2 mb-4">
-                    <div className="flex flex-col gap-5 items-center">
-                        <h1 className="title text-black">Contact Us</h1>
-                        <div className="p-3 w-full md:w-3/4 mx-auto">
-                            <form
-                                ref={form}
-                                onSubmit={sendEmail}
-                                className="flex flex-col"
-                            >
-                                <div className="flex flex-col md:flex-row">
+                <main className="flex flex-col items-center justify-center min-h-screen py-12">
+                    <div className="flex flex-col md:flex-row w-11/12 gap-6 max-w-6xl p-8 space-y-8 bg-gray-700 rounded-lg shadow-md">
+                        <div className="w-full md:w-1/2 space-y-8">
+                            <h2 className="text-3xl font-bold text-center">Contact Us</h2>
+                            <form className="space-y-6" ref={form} onSubmit={sendEmail}>
+                                <div className="space-y-2">
+                                    <label className="block text-sm font-medium text-primaryText" htmlFor="name">
+                                        Name
+                                    </label>
                                     <input
-                                        type="text"
-                                        name="user_name"
-                                        placeholder="Your Name"
-                                        className="input-form"
-                                        required
-                                    />
-                                    <input
-                                        type="text"
-                                        name="user_contact"
-                                        placeholder="Your Phone"
-                                        className="input-form"
-                                        required
+                                    required
+                                        className="block w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md border border-gray-300 focus:outline-none focus:ring-primaryText focus:border-primaryText sm:text-sm"
+                                        id="user_name"
+                                        placeholder="Enter your name"
                                     />
                                 </div>
-                                <div>
+                                <div className="space-y-2">
+                                    <label className="block text-sm font-medium text-primaryText" htmlFor="phone">
+                                        Phone
+                                    </label>
                                     <input
-                                        type="email"
+                                    required
+                                        className="block w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md border border-gray-300 focus:outline-none focus:ring-primaryText focus:border-primaryText sm:text-sm"
+                                        id="user_contact"
+                                        placeholder="Enter your phone number"
+                                    />
+                                </div>
+                                <div className="space-y-2">
+                                    <label className="block text-sm font-medium text-primaryText" htmlFor="email">
+                                        Email
+                                    </label>
+                                    <input
+                                    required
+                                        className="block w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md border border-gray-300 focus:outline-none focus:ring-primaryText focus:border-primaryText sm:text-sm"
+                                        id="email"
                                         name="user_email"
-                                        placeholder="Your Email"
-                                        className="input-form"
-                                        required
+                                        placeholder="Enter your email"
+                                        type="email"
                                     />
                                 </div>
-
-
-                                <div className="flex flex-col md:flex-row">
+                                <div className="space-y-2">
+                                    <label className="block text-sm font-medium text-primaryText" htmlFor="service">
+                                        Service
+                                    </label>
                                     <select
+                                        className="block w-full px-3 py-2 text-gray-700 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-primaryText focus:border-primaryText sm:text-sm"
+                                        id="service"
                                         name="service"
                                         required
-                                        className="input-form"
                                     >
-                                        <option value="None">{"<"}Select{">"}</option>
+                                        <option value="None">Select</option>
                                         <option value="Campaigning">Campaigning</option>
                                         <option value="Social Media Marketing">Social Media Marketing</option>
                                         <option value="Website Development">Website Development</option>
@@ -105,90 +113,81 @@ const Contact = () => {
                                         <option value="Videography">Videography</option>
                                         <option value="Others">Others</option>
                                     </select>
+                                </div>
+                                <div className="space-y-2">
+                                    <label className="block text-sm font-medium text-primaryText" htmlFor="organization">
+                                        Organization/Company
+                                    </label>
                                     <input
-                                        type="text"
+                                    required
                                         name="purpose"
-                                        placeholder="For? Like Company, Political Party etc."
-                                        className="input-form"
-                                        required
+                                        className="block w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md border border-gray-300 focus:outline-none focus:ring-primaryText focus:border-primaryText sm:text-sm"
+                                        id="organization"
+                                        placeholder="Enter your organization or company"
                                     />
                                 </div>
-                                <textarea
-                                    name="message"
-                                    placeholder="Your Message"
+                                <div className="space-y-2">
+                                    <label className="block text-sm font-medium text-primaryText" htmlFor="message">
+                                        Message
+                                    </label>
+                                    <textarea
                                     required
-                                    className="input-form"
-                                />
+                                        className="min-h-[100px] block w-full px-3 py-2 placeholder-gray-500 text-gray-900 rounded-md border border-gray-300 focus:outline-none focus:ring-primaryText focus:border-primaryText sm:text-sm"
+                                        id="message"
+                                        placeholder="Enter your message"
+                                        name="message"
+                                    />
+                                </div>
                                 <button
-                                    className="bg-orange-500 px-4 py-2 w-1/2 md:w-1/3 mx-auto rounded-full text-secondaryText mt-2 transition duration-200 ease-in-out hover:bg-secondaryText hover:text-bg hover:scale-95"
+                                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primaryText hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                     type="submit"
                                 >
-                                    Submit
+                                    Send message
                                 </button>
                             </form>
-                            <div className="alert"></div>
                         </div>
-                        <div className="flex items-center text-4xl gap-4 mt-3 p-2">
-                            <Link
-                                href="https://github.com/prnvtripathi/campaigning-source"
-                                className="transition-all ease-in-out duration-200 hover:text-gray-500 hover:scale-125"
-                            >
-                                <FaGithub />
-                            </Link>
-                            <Link
-                                href="/"
-                                className="transition-all ease-in-out duration-200 hover:text-blue-500 hover:scale-125"
-                            >
-                                <FaTwitter />
-                            </Link>
-                            <Link
-                                href="/"
-                                className="transition-all ease-in-out duration-200 hover:text-blue-500 hover:scale-125"
-                            >
-                                <FaFacebook />
-                            </Link>
-                            <Link
-                                href="/"
-                                className="transition-all ease-in-out duration-200 hover:text-purple-500 hover:scale-125"
-                            >
-                                <FaInstagram />
-                            </Link>
+                        <div className="w-full md:w-1/2">
+                            <img
+                                alt="Contact Us"
+                                className="w-full h-full object-cover rounded-lg"
+                                src="/contact-us.svg"
+                            />
                         </div>
                     </div>
-                    <div className="flex flex-col justify-center items-center gap-3">
-                        <div className="outline bg-blue-200 bg-opacity-40 rounded px-3 w-full md:w-1/2 h-auto md:h-1/4">
-                            <h1 className="text-xl text-black mb-5">Address:</h1>
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.87140791622!2d77.44511047522582!3d28.63361588403194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cee3d4e3485ed%3A0xe0fe1689b57c7d2e!2sABESIT%20GROUP%20OF%20INSTITUTIONS!5e0!3m2!1sen!2sin!4v1691946892434!5m2!1sen!2sin"
-                                width="100%"
-                                height="60%"
-                                allowFullScreen=""
-                                loading="lazy"
-                                referrerPolicy="no-referrer-when-downgrade"
-                                className="outline outline-black my-2 rounded-md"
-                            ></iframe>
+                    <div className="w-11/12 max-w-6xl p-8 mt-12 space-y-4 text-center bg-gray-700 rounded-lg shadow-md flex flex-col md:flex-row justify-around items-center">
+                        <div className="flex flex-col items-center justify-center gap-2">
+                            <h3 className="text-3xl font-bold">Follow Us</h3>
+                            <div className="flex justify-center space-x-4 text-2xl">
+                                <Link className="text-blue-600 hover:text-blue-800" href="#">
+                                    <FaFacebook />
+                                </Link>
+                                <Link className="text-blue-400 hover:text-blue-600" href="#">
+                                    <FaTwitter />
+                                </Link>
+                                <Link className="text-pink-600 hover:text-pink-800" href="#">
+                                    <FaInstagram />
+                                </Link>
+                                <Link className="text-blue-700 hover:text-blue-900" href="#">
+                                    <FaLinkedinIn />
+                                </Link>
+                            </div>
                         </div>
-                        <div className="outline outline-orange-300 bg-orange-100 bg-opacity-40 rounded py-3 w-full md:w-1/2 px-3 h-auto md:h-1/4">
-                            <h1 className="text-xl text-black mb-5">Phone:</h1>
-                            <p className="text-gray-600 mb-3">
-                                Assistance Hours:<br /> Monday - Friday, 9AM to 5PM
-                            </p>
-                            <p className="text-black">+91 82874 04804</p>
+                        <div className="h-0 border-0 md:h-24 md:border md:border-1 md:border-gray-500"></div>
+                        <div>
+                            <h3 className="text-3xl font-bold">Phone</h3>
+                            <p className="text-gray-400">+91 82874 04804</p>
                         </div>
-                        <div className="outline outline-green-600 bg-green-200 bg-opacity-40 rounded py-3 w-full md:w-1/2 px-3 h-auto md:h-1/4">
-                            <h1 className="text-xl text-black mb-5">Email:</h1>
-                            <p className="text-gray-600 mb-3">
-                                Our team will get back to you in 24 business hours
+                        <div className="h-0 border-0 md:h-24 md:border md:border-1 md:border-gray-500"></div>
+                        <div>
+                            <h3 className="text-3xl font-bold">Email</h3>
+                            <p className="text-gray-400">
+                                <Link className="text-gray-400 hover:text-blue-300" href="mailto:info@campaigningsource.com">
+                                    info@campaigningsource.com
+                                </Link>
                             </p>
-                            <Link
-                                href="mailto:info@campaigningsource.com"
-                                className="text-black underline"
-                            >
-                                info@campaigningsource.com
-                            </Link>
                         </div>
                     </div>
-                </div>
+                </main>
             </Layout>
         </>
     );
