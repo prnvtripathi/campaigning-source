ec9a3672324bd884f6b32eb73efa5caaf64ccfae
Pranav Tripathi <tripathipranav14@gmail.com>
2024-01-15 11:57:13 +0530

    Add Whatsapp chat functionality

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,36 +13,12 @@ const Footer = () => {
   const year = date.getFullYear();
 
   return (
-    // <footer className=" bg-gradient-to-r from-cyan-400 to-sky-300 text-center rounded-md text-black py-6">
-    //     <div className="flex flex-col md:flex-row justify-center md:justify-between items-center px-12 pt-2">
-    //         <Link href='/'>
-    //             <Image src="/logo.png" alt="logo" width={200} height={200} />
-    //         </Link>
-    //         <div className="mt-4 md:mt-0">
-    //             <div className="text-center flex flex-col md:flex-row gap-3 justify-evenly mb-4 md:mb-0">
-    //                 <div className="hover:underline"><Link href="/">Home</Link></div>
-    //                 <div className="hover:underline"><Link href="/contact">Influencers</Link></div>
-    //                 <div className="hover:underline"><Link href="/about">About Us</Link></div>
-    //                 <div className="hover:underline"><Link href="/contact">Contact</Link></div>
-    //                 <div className="hover:underline"><Link href="https://myfameguru.com/">Social Media Boost</Link></div>
-    //             </div>
-    //         </div>
-    //     </div>
-    //     <div className="flex flex-col md:flex-row justify-center items-center md:items-end">
-    //         <div className="flex items-center text-2xl gap-4 mt-2 px-2">
-    //             <Link target="_blank" href="https://www.instagram.com/campaigningsource/" className="transition hover:text-purple-500"><FaInstagram /></Link>
-    //             <Link target="_blank" href="https://www.linkedin.com/company/100733755/" className="transition hover:text-blue-600"><FaLinkedin /></Link>
-    //         </div>
-    //     </div>
-    //     <p className="py-4 select-none">© {year} Campaigning Source</p>
-    // </footer>
-
-    <footer className="bg-transparent bg-gradient-to-t from-blue-500/10 to-transparent">
+    <footer className="bg-transparent bg-gradient-to-t from-blue-500/20 to-transparent">
       <div className="mx-auto relative max-w-screen-xl px-4 py-10 md:px-10">
         <div className="absolute end-4 top-4 sm:end-6 sm:top-6 lg:end-8 lg:top-8">
           <Link
             href="#"
-            className="inline-block rounded-full bg-teal-600 p-2 text-white shadow transition hover:bg-teal-500 dark:bg-gray-700 dark:text-teal-300 dark:hover:bg-gray-600 sm:p-3 lg:p-4"
+            className="inline-block rounded-full bg-primaryText p-2 text-white shadow transition hover:bg-indigo-500 sm:p-3 lg:p-4"
           >
             <span className="sr-only">Back to top</span>
             <FaChevronUp />
@@ -62,7 +38,7 @@ const Footer = () => {
             </div>
 
             <div className="lg:text-right">
-              <p className="mx-auto mt-6 max-w-md text-center md:text-left leading-relaxed text-gray-500 dark:text-gray-400">
+              <p className="mx-auto mt-6 max-w-md text-center md:text-left leading-relaxed text-gray-300">
                 Your one-stop solution for political promotion and business
                 development. We help you navigate the political landscape and grow
                 your business.
@@ -73,7 +49,7 @@ const Footer = () => {
             <ul className="mt-12 flex flex-wrap justify-center lg:justify-end gap-6 md:gap-8 lg:gap-12">
               <li>
                 <a
-                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-300 transition hover:text-gray-300/75"
                   href="/"
                 >
                   About
@@ -81,7 +57,7 @@ const Footer = () => {
               </li>
               <li>
                 <a
-                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-300 transition hover:text-gray-300/75"
                   href="/"
                 >
                   Contact
@@ -89,7 +65,7 @@ const Footer = () => {
               </li>
               <li>
                 <a
-                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-300 transition hover:text-gray-300/75"
                   href="/"
                 >
                   Pricing
@@ -97,7 +73,7 @@ const Footer = () => {
               </li>
               <li>
                 <a
-                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-300 transition hover:text-gray-300/75"
                   href="/"
                 >
                   Swags
@@ -109,7 +85,7 @@ const Footer = () => {
         <hr className="border-gray-600 mb-4" />
         <div className="flex flex-col-reverse md:flex-row justify-between items-center">
           <div>
-            <p className="text-center text-gray-500 dark:text-gray-400">
+            <p className="text-center text-gray-400">
               © {year} Campaigning Source. All rights reserved.
             </p>
           </div>
@@ -120,7 +96,7 @@ const Footer = () => {
                   href="/"
                   rel="noreferrer"
                   target="_blank"
-                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-400 transition hover:text-blue-500"
                 >
                   <span className="sr-only">Facebook</span>
                   <FaFacebook className="h-4-w-4" />
@@ -131,7 +107,7 @@ const Footer = () => {
                   href="https://www.instagram.com/campaigningsource/"
                   rel="noreferrer"
                   target="_blank"
-                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-400 transition hover:text-purple-500"
                 >
                   <span className="sr-only">Instagram</span>
                   <FaInstagram className="h-4-w-4" />
@@ -142,7 +118,7 @@ const Footer = () => {
                   href="/"
                   rel="noreferrer"
                   target="_blank"
-                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-400 transition hover:text-gray-50"
                 >
                   <span className="sr-only">Twitter</span>
                   <FaXTwitter className="h-4-w-4" />
@@ -153,7 +129,7 @@ const Footer = () => {
                   href="https://www.linkedin.com/company/100733755/"
                   rel="noreferrer"
                   target="_blank"
-                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-400 transition hover:text-blue-600"
                 >
                   <span className="sr-only">LinkedIn</span>
                   <FaLinkedin className="h-4-w-4" />
