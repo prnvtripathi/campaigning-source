b64376d878449462df59772987252a8b038303d8
Pranav Tripathi <tripathipranav14@gmail.com>
2024-01-15 11:41:17 +0530

    Refactor Footer component and remove Chatbot component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -38,36 +38,38 @@ const Footer = () => {
     // </footer>
 
     <footer className="bg-transparent bg-gradient-to-t from-blue-500/10 to-transparent">
-      <div className="mx-auto relative max-w-screen-xl px-4 py-10 sm:px-6 lg:px-8">
+      <div className="mx-auto relative max-w-screen-xl px-4 py-10 md:px-10">
         <div className="absolute end-4 top-4 sm:end-6 sm:top-6 lg:end-8 lg:top-8">
           <Link
             href="#"
-            class="inline-block rounded-full bg-teal-600 p-2 text-white shadow transition hover:bg-teal-500 dark:bg-gray-700 dark:text-teal-300 dark:hover:bg-gray-600 sm:p-3 lg:p-4"
+            className="inline-block rounded-full bg-teal-600 p-2 text-white shadow transition hover:bg-teal-500 dark:bg-gray-700 dark:text-teal-300 dark:hover:bg-gray-600 sm:p-3 lg:p-4"
           >
             <span className="sr-only">Back to top</span>
             <FaChevronUp />
           </Link>
         </div>
+        <div className="flex flex-col md:flex-row justify-between items-center mb-4">
+          <div className="lg:flex lg:justify-between">
+            <div className="flex justify-center md:justify-start mb-8 lg:mb-0">
+              <Link href="/">
+                <Image
+                  src="/logo-white.png"
+                  alt="logo"
+                  width={200}
+                  height={200}
+                />
+              </Link>
+            </div>
 
-        <div className="lg:flex lg:justify-between">
-          <div className="flex justify-center lg:justify-start mb-8 lg:mb-0">
-            <Link href="/">
-              <Image
-                src="/logo-white.png"
-                alt="logo"
-                width={200}
-                height={200}
-              />
-            </Link>
+            <div className="lg:text-right">
+              <p className="mx-auto mt-6 max-w-md text-center md:text-left leading-relaxed text-gray-500 dark:text-gray-400">
+                Your one-stop solution for political promotion and business
+                development. We help you navigate the political landscape and grow
+                your business.
+              </p>
+            </div>
           </div>
-
-          <div className="lg:text-right">
-            <p className="mx-auto mt-6 max-w-md text-center lg:text-left leading-relaxed text-gray-500 dark:text-gray-400">
-              Your one-stop solution for political promotion and business
-              development. We help you navigate the political landscape and grow
-              your business.
-            </p>
-
+          <div>
             <ul className="mt-12 flex flex-wrap justify-center lg:justify-end gap-6 md:gap-8 lg:gap-12">
               <li>
                 <a
@@ -104,53 +106,63 @@ const Footer = () => {
             </ul>
           </div>
         </div>
+        <hr className="border-gray-600 mb-4" />
+        <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+          <div>
+            <p className="text-center text-gray-500 dark:text-gray-400">
+              © {year} Campaigning Source. All rights reserved.
+            </p>
+          </div>
+          <div>
+            <ul className="flex justify-center lg:justify-end gap-6 md:gap-8">
+              <li>
+                <a
+                  href="/"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                >
+                  <span className="sr-only">Facebook</span>
+                  <FaFacebook className="h-4-w-4" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/campaigningsource/"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                >
+                  <span className="sr-only">Instagram</span>
+                  <FaInstagram className="h-4-w-4" />
+                </a>
+              </li>
+              <li>
+                <Link
+                  href="/"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                >
+                  <span className="sr-only">Twitter</span>
+                  <FaXTwitter className="h-4-w-4" />
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.linkedin.com/company/100733755/"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                >
+                  <span className="sr-only">LinkedIn</span>
+                  <FaLinkedin className="h-4-w-4" />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
 
-        <ul className="mt-12 flex justify-center lg:justify-end gap-6 md:gap-8">
-          <li>
-            <a
-              href="/"
-              rel="noreferrer"
-              target="_blank"
-              className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
-            >
-              <span className="sr-only">Facebook</span>
-              <FaFacebook className="h-6 w-6" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/campaigningsource/"
-              rel="noreferrer"
-              target="_blank"
-              className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
-            >
-              <span className="sr-only">Instagram</span>
-              <FaInstagram className="h-6 w-6" />
-            </a>
-          </li>
-          <li>
-            <Link
-              href="/"
-              rel="noreferrer"
-              target="_blank"
-              className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
-            >
-              <span className="sr-only">Twitter</span>
-              <FaXTwitter className="h-6 w-6" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://www.linkedin.com/company/100733755/"
-              rel="noreferrer"
-              target="_blank"
-              className="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
-            >
-              <span className="sr-only">LinkedIn</span>
-              <FaLinkedin className="h-6 w-6" />
-            </Link>
-          </li>
-        </ul>
       </div>
     </footer>
   );
