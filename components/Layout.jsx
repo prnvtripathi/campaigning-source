06e9dc418fcdaf6117b580fca417c4f0932f522b
Pranav Tripathi <tripathipranav14@gmail.com>
2024-01-19 10:12:18 +0530

    Add Searchbar component to Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,13 @@
 import Footer from "./Footer"
 import Chatbot from "./Chatbot"
 import Navbar from "./Navbar"
+import Searchbar from "./Searchbar"
 
 const Layout = ({ children }) => {
     return (
         <>
             <Navbar />
+            <Searchbar />
             {children}
             <Footer />
             {/* <Chatbot /> */}
