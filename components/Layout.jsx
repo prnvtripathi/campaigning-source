3d74ec67e70c67cb7e5e797d52bcf2362123c3bd
Pranav Tripathi <tripathipranav14@gmail.com>
2024-01-22 14:43:38 +0530

    Add cookie notification to Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
 import Footer from "./Footer"
 import Chatbot from "./Chatbot"
 import Navbar from "./Navbar"
 import Searchbar from "./Searchbar"
+import Cookies from "./Cookies"
 
 const Layout = ({ children }) => {
+    const [isCookieVisible, setCookieVisible] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCookieVisible(true);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    const closeDialog = () => {
+        setCookieVisible(false);
+    };
     return (
         <>
             <Navbar />
@@ -11,6 +25,8 @@ const Layout = ({ children }) => {
             {children}
             <Footer />
             {/* <Chatbot /> */}
+            {isCookieVisible && <Cookies closeDialog={closeDialog} />}
+
         </>
     )
 }
