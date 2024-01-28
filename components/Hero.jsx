6ade62253924ef8f614676e0ab50f243361126c3
Ishaan2053 <Ishaan2053@gmail.com>
2024-01-28 23:41:10 +0530

    Update Hero component styling

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -19,13 +19,13 @@ export default function Component() {
           transition={{ duration: 0.5, type: "inertia", velocity: 50 }}
           className="flex items-center justify-center text-center md:text-left w-full px-8 py-6 md:h-[41rem] md:w-1/2">
           <div className="max-w-2xl">
-            <h2 className="text-3xl font-semibold text-gray-300 md:text-4xl">Make your startup a<br /><span className={`text-primaryText font-bold md:text-5xl ${satisfy.className}`}>Brand</span></h2>
+            <h2 className="text-3xl font-semibold text-gray-300 tracking-widest md:text-4xl">Make Your Startup a<br /><span className={`text-primaryText font-bold md:text-5xl ${satisfy.className}`}>Brand</span></h2>
 
             <p className="mt-4 text-sm text-gray-500 md:text-base">Your one-stop solution for political promotion and business development. We help you navigate the political landscape and grow your business.</p>
 
             <div className="flex flex-col mt-6 gap-2 md:flex-row">
-              <Link href="/services" className="px-5 py-2 text-sm font-medium text-center text-white bg-primaryText/50 rounded-md hover:bg-primaryText/70">Get Started</Link>
-              <Link href="/about" className="px-5 py-2 text-sm font-medium text-center text-gray-700 bg-gray-200 rounded-md md:mx-4 hover:bg-gray-300">Learn More</Link>
+              <Link href="/services" className="px-5 py-2 text-sm font-medium text-center text-white transition bg-primaryText/50 rounded-md hover:bg-primaryText/70">Get Started</Link>
+              <Link href="/about" className="px-5 py-2 text-sm font-medium text-center text-gray-100 transition hover:text-gray-800 border border-gray-200 rounded-md md:mx-4 hover:bg-gray-300">Learn More</Link>
             </div>
           </div>
         </motion.div>
@@ -37,7 +37,7 @@ export default function Component() {
           transition={{ duration: 0.5, type: "inertia", velocity: 50 }}
           className="w-full md:w-1/2 mx-auto p-2">
           <div className="w-full h-full mx-auto">
-            <Image src="/hero.webp" width={1000} height={1000} alt="hero image" className="opacity-70 mx-auto" />
+            <Image src="/hero.webp" width={1000} height={1000} alt="hero image" className="opacity-70 rounded  mx-auto" />
           </div>
         </motion.div>
       </section>
