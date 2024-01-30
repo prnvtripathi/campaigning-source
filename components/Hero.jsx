524636f87e279259c5437482f11f0dff31cd6017
Pranav Tripathi <tripathipranav14@gmail.com>
2024-01-30 23:46:57 +0530

    Update Hero component styling

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -19,8 +19,8 @@ export default function Component() {
           viewport={{ once: true }}
           transition={{ duration: 0.5, type: "inertia", velocity: 50 }}
           className="flex items-center justify-center text-center md:text-left w-full px-8 py-6 md:h-[41rem] md:w-1/2">
-          <div className="max-w-2xl">
-            <h2 className="text-3xl font-semibold text-gray-300 tracking-widest md:text-4xl">Make Your Startup a<br /><span className={`text-primaryText font-bold md:text-5xl ${satisfy.className}`}>Brand</span></h2>
+          <div className="max-w-2xl mb-28">
+            <h2 className="text-3xl font-semibold text-gray-300 tracking-widest md:text-5xl">Make Your Startup a<br /><span className={`text-primaryText font-bold md:text-7xl ${satisfy.className}`}>Brand</span></h2>
 
             <p className="mt-4 text-sm text-gray-500 md:text-base">Your one-stop solution for political promotion and business development. We help you navigate the political landscape and grow your business.</p>
 
