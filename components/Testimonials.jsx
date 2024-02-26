83fa6075d228e410fb3a5c5b6c0cb92a27aaf0f9
Pranav Tripathi <tripathipranav14@gmail.com>
2024-02-27 00:13:51 +0530

    Services page revamp

---
+++ components/Testimonials.jsx
@@ -0,0 +1,204 @@
+import FiveStars from "./FiveStars";
+
+export default function Testimonials() {
+    return (
+        <section className="">
+            <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+                <h2 className="text-center text-4xl font-bold tracking-tight sm:text-5xl">
+                    What our clients say
+                </h2>
+
+                <div className="mt-8 [column-fill:_balance] sm:columns-2 sm:gap-6 md:columns-3 md:gap-8">
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a-/ALV-UjXAw3qjgjz0BhwTL-XIkIipzifxh1trIiBzKtutPlAgLQ=w75-h75-p-rp-mo-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">Shreekant Tyagi</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                We had an amazing experience with Campaigning Source! Their broadcast services and digital marketing really helped our campaigns. The team's expertise and hard work boosted our online presence, and we saw great results.
+                            </p>
+                        </blockquote>
+                    </div>
+
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a/ACg8ocKHto-SeSbCix94kTga68yOjm_QLa3xnjrG5RvUejxf=w75-h75-p-rp-mo-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">FABRIC CLINIC</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                I can't say all the good things about Campaigning Source! They deserve every star for their exceptional services. Not only did they consistently deliver top-notch work on time, but their expertise in every field is truly remarkable.
+                            </p>
+                        </blockquote>
+                    </div>
+
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a/ACg8ocIANVEBZ_5rS2QjVqSWpyrbScUSWGF61yfogIuKnYb2=w75-h75-p-rp-mo-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">Diksha Sharma</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                Campaigning Source has transformed our hospital's website, elevating accessibility and user experience. Highly recommended for healthcare institutions seeking comprehensive digital solutions.
+                            </p>
+                        </blockquote>
+                    </div>
+
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a-/ALV-UjX1ryjZH_ld2FQLNTeIC44RGRJVJAfUUBTPkz0_yeK6SCM=w75-h75-p-rp-mo-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">Kutch Prayas</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                We extend our heartfelt thanks to Campaigning Source for their generous donation of food and funds. Their support has made a significant impact, alleviating hunger in our community and empowering us to extend our reach.
+                            </p>
+                        </blockquote>
+                    </div>
+
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a-/ALV-UjUout2_TpMPXXMR9WWR4GmCzJ_sBEuc-ORSxkrFOo0HI9LK=w75-h75-p-rp-mo-ba2-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">Ojha Baba</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                Our experience with their TV Ads service was just amazing! They understood our brand and created a commercial that perfectly captured our message. It's no wonder our ad campaign saw such great results. Kudos to the team!
+                            </p>
+                        </blockquote>
+                    </div>
+
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a-/ALV-UjVBM2Bg34qB28_YcnA1VrQzGzocCSZ8JVdyvt7fTdaV2A=w75-h75-p-rp-mo-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">Aliya Sharma</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                Partnering with this company for our marketing needs has been a game-changer! The team's creativity and dedication shine through in every campaign, making our collaboration both productive and enjoyable.
+                            </p>
+                        </blockquote>
+                    </div>
+
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a-/ALV-UjWdeQlLEALXJFYxzCS3caF_Xwj7wMKaYqECtXhCuKGU-A=w75-h75-p-rp-mo-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">Sneha Pandey</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                Let me share my personal experience with Campaigning Source and how it has been a game-changer for my Instagram ads. The targeting options are fantastic, allowing me to reach the right audience. The analytics and reporting have helped me optimize my campaigns for better results. Overall, I highly recommend Campaigning Source.
+                            </p>
+                        </blockquote>
+                    </div>
+
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a/ACg8ocI2wurNHfBhT8TGVpEIwXi7JEx8k9_zA0sOh5sOW0_zQw=w75-h75-p-rp-mo-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">Riya Goel</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                Wow, I can't thank the team enough for the incredible boost they've given to our online presence! From revamping our Google Business Profile to managing our Facebook Ads.
+                            </p>
+                        </blockquote>
+                    </div>
+
+                    <div className="mb-8 sm:break-inside-avoid">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                            <div className="flex items-center gap-4">
+                                <img
+                                    alt="customer image"
+                                    src="https://lh3.googleusercontent.com/a-/ALV-UjXPVWkD0rYUbBF3lBLAYa-wdNpumqtSayiQHukIxJIU7Q=w75-h75-p-rp-mo-br100"
+                                    className="size-14 rounded-full object-cover"
+                                />
+
+                                <div>
+                                    <p className="mt-0.5 text-lg font-medium text-white">Aditi Tyagi</p>
+                                    <FiveStars />
+                                </div>
+                            </div>
+
+                            <p className="mt-4 text-gray-400">
+                                Extremely glad with the graphics Campaigning Source created for our social media! They really caught the eye and boosted our online presence.
+                            </p>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
--- pages/services/[id].js
+++ pages/services/[id].js
@@ -5,6 +5,7 @@ import services from "@/data/services"
 import { useState } from "react"
 import Banner from "@/components/Banner"
 import GraphicsCarousel from "@/components/GraphicsCarousel"
+import Testimonials from "@/components/Testimonials"
 
 export default function ServicePage({ service }) {
 
@@ -24,13 +25,13 @@ export default function ServicePage({ service }) {
             </Head>
             <Layout>
                 <main className="w-full">
-                    <section className="md:w-11/12 mx-auto p-10 md:p-20">
+                    <section className="md:w-11/12 mx-auto py-12 md:p-20">
                         <div className="px-4 md:px-6">
-                            <div className="flex flex-col md:flex-row">
+                            <div className="flex flex-col md:flex-row gap-12">
                                 <div className="flex flex-col justify-center space-y-4">
                                     <div className="space-y-2">
                                         <h1 className="text-3xl font-bold tracking-wide md:text-5xl text-center md:text-left">{service.title}</h1>
-                                        <p className="max-w-[600px] text-gray-500 md:text-lg text-center md:text-left">
+                                        <p className="text-gray-500 max-w-xl md:text-lg text-center md:text-left">
                                             {displayDescription}
                                             {service.desc.length > 300 && (
                                                 <span onClick={toggleDescription} className="text-gray-300 text-xs">
@@ -38,14 +39,14 @@ export default function ServicePage({ service }) {
                                                 </span>
                                             )}
                                         </p>
-                                        <div className="my-4">
+                                        {/* <div className="my-4">
                                             <Link
                                                 className="inline-flex h-9 items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium text-gray-900 shadow transition-colors hover:bg-gray-100 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50"
                                                 href="/contact"
                                             >
                                                 Contact Sales
                                             </Link>
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </div>
                                 <img
@@ -61,66 +62,83 @@ export default function ServicePage({ service }) {
                         <section className="bg-gray-800 mb-12">
                             <h2 className="text-3xl font-bold tracking-wide md:text-5xl text-center py-3">Examples</h2>
                             <div className="py-2">
-                            <GraphicsCarousel />
+                                <GraphicsCarousel />
                             </div>
                         </section>
                     ) : null}
-                    {service.why ?
+
+                    {service.what ?
                         (
                             <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-800">
                                 <div className="px-4 md:px-6">
                                     <div className="flex flex-col items-center justify-center space-y-4 text-center">
                                         <div className="space-y-2 flex flex-col gap-3">
-                                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Why Choose Us</h2>
-                                            <p className="max-w-4xl text-gray-500">
-                                                {service.why}
-                                            </p>
+                                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mb-2">
+                                                What We Offer
+                                            </h2>
+                                            <div className="flex flex-wrap w-5/6 mx-auto gap-4 justify-center">
+                                                {Object.entries(service.what).map(([title, description]) => (
+                                                    <div key={title} className="flex flex-col gap-4 w-80 items-center justify-center min-h-40 py-3 px-2 bg-gray-900 rounded-2xl hover:bg-gray-900/70 hover:scale-95 transition-all">
+                                                        <h3 className="text-xl font-bold text-gr">{title}</h3>
+                                                        <p className="text-gray-300">{description}</p>
+                                                    </div>
+                                                ))}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </section>
                         )
-                        :
+                        : null}
+
+                    {service.where && (
+                        <section className="w-full py-12 md:py-24 lg:py-32">
+                            <div className="px-4 md:px-6">
+                                <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                                    <div className="space-y-2 flex flex-col gap-3">
+                                        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mb-2">
+                                            Where We Deliver
+                                        </h2>
+                                        <div className="flex flex-wrap w-5/6 mx-auto gap-4 justify-center">
+                                            <p className="text-gray-500 text-xl">
+                                                {service.where}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    )}
+
+                    {service.benefits ?
                         (
                             <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-800">
                                 <div className="px-4 md:px-6">
                                     <div className="flex flex-col items-center justify-center space-y-4 text-center">
-                                        <div className="space-y-2">
-                                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Why Choose Us</h2>
-                                            <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                                                We provide the best service in the industry. Our team is dedicated to ensuring customer satisfaction and
-                                                we strive to exceed expectations.
-                                            </p>
+                                        <div className="space-y-2 flex flex-col gap-3">
+                                            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mb-2">
+                                                Benefits you get
+                                            </h2>
+                                            <div className="flex flex-wrap w-5/6 mx-auto gap-4 justify-center">
+                                                {Object.entries(service.benefits).map(([title, description]) => (
+                                                    <div key={title} className="flex flex-col gap-4 w-80 items-center justify-center min-h-40 py-3 px-2 outline-gray-300 outline rounded-2xl hover:bg-gray-900/70 hover:scale-95 transition-all">
+                                                        <h3 className="text-xl font-bold text-gr">{title}</h3>
+                                                        <p className="text-gray-300">{description}</p>
+                                                    </div>
+                                                ))}
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </section>
                         )
-                    }
-                    <section className="w-full py-12 md:py-24 lg:py-32">
-                        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 lg:gap-10">
-                            <div className="space-y-3">
-                                <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">Testimonials</h2>
-                                <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                                    Hear from our satisfied customers.
-                                </p>
-                            </div>
-                            <div className="grid w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-center gap-8">
-                                <div className="p-4 border rounded shadow">
-                                    <p>"This service is amazing! It has made my life so much easier."</p>
-                                    <p>- Happy Customer</p>
-                                </div>
-                                <div className="p-4 border rounded shadow">
-                                    <p>"I can't imagine going back to how things were before. This service is a game changer."</p>
-                                    <p>- Satisfied User</p>
-                                </div>
-                                <div className="p-4 border rounded shadow">
-                                    <p>"The team behind this service is fantastic. They've been so helpful and responsive."</p>
-                                    <p>- Grateful Client</p>
-                                </div>
-                            </div>
-                        </div>
+                        : null}
+
+                    <section>
+                        <Testimonials />
                     </section>
+
+
                     <section>
                         <Banner />
                     </section>
