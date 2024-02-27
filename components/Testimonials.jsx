b2a6b5529a56e84ff62af8a34e66bc8e7d1f137c
Pranav Tripathi <tripathipranav14@gmail.com>
2024-02-27 14:27:11 +0530

    Update testimonial blockquote styles

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -10,7 +10,7 @@ export default function Testimonials() {
 
                 <div className="mt-8 [column-fill:_balance] sm:columns-2 sm:gap-6 md:columns-3 md:gap-8">
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
@@ -31,7 +31,7 @@ export default function Testimonials() {
                     </div>
 
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
@@ -52,7 +52,7 @@ export default function Testimonials() {
                     </div>
 
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
@@ -73,7 +73,7 @@ export default function Testimonials() {
                     </div>
 
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
@@ -94,7 +94,7 @@ export default function Testimonials() {
                     </div>
 
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
@@ -115,7 +115,7 @@ export default function Testimonials() {
                     </div>
 
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
@@ -136,7 +136,7 @@ export default function Testimonials() {
                     </div>
 
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
@@ -157,7 +157,7 @@ export default function Testimonials() {
                     </div>
 
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
@@ -178,7 +178,7 @@ export default function Testimonials() {
                     </div>
 
                     <div className="mb-8 sm:break-inside-avoid">
-                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8">
+                        <blockquote className="rounded-lg bg-gray-700 p-6 shadow-sm sm:p-8 hover:bg-gray-700/70 transition">
                             <div className="flex items-center gap-4">
                                 <img
                                     alt="customer image"
