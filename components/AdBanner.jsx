57daf39421c3b3c2ca21107d5436c6ffe112e1a9
Pranav Tripathi <tripathipranav14@gmail.com>
2024-03-17 11:36:34 +0530

    Update AdBanner text for startup registration

--- components/AdBanner.jsx
+++ components/AdBanner.jsx
@@ -4,7 +4,7 @@ export default function AdBanner() {
     return (
         <div className="w-full py-1 text-center bg-primaryText/80 text-white">
             <p className='font-semibold font-sans'>
-                ✨ Want to turn your idea into a startup? we'll help you here. {" "}
+                ✨ Having some problems in registering your startup? {" "}
                 <Link href="/company" className='underline text-black'>
                     click here!
                 </Link>
